0446bf23aee57f586c9738d9f870eec2f7c437b4
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-11-27 10:20:23 +0200

    customization for the discovery page

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { CategoryRow } from "@/components/CategoryRow";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { Pencil } from "lucide-react";
+import { DiscoverCustomization } from "@/components/DiscoverCustomization";
 
-const CATEGORIES = [
+let CATEGORIES = [
   { title: "Trending", query: "trending" },
   { title: "Never Aging", query: "never aging" },
   { title: "Pop", query: "pop" },
@@ -11,16 +15,59 @@ const CATEGORIES = [
 ];
 
 export default function Page() {
+  // All of the current field stuff. Replace later with user based info
+  const searchParams = useSearchParams();
+  const field1 = searchParams.get("field1");
+  const field2 = searchParams.get("field2");
+  const field3 = searchParams.get("field3");
+  const field4 = searchParams.get("field4");
+  const field5 = searchParams.get("field5");
+  const query1 = searchParams.get("query1");
+  const query2 = searchParams.get("query2");
+  const query3 = searchParams.get("query3");
+  const query4 = searchParams.get("query4");
+  const query5 = searchParams.get("query5");
+
+  if (field1 && query1 && field1?.length > 0) {
+    CATEGORIES[0].title = field1;
+    CATEGORIES[0].query = query1;
+  }
+  if (field2 && query2 && field2?.length > 0) {
+    CATEGORIES[1].title = field2;
+    CATEGORIES[1].query = query2;
+  }
+  if (field3 && query3 && field3?.length > 0) {
+    CATEGORIES[2].title = field3;
+    CATEGORIES[2].query = query3;
+  }
+  if (field4 && query4 && field4?.length > 0) {
+    CATEGORIES[3].title = field4;
+    CATEGORIES[3].query = query4;
+  }
+  if (field5 && query5 && field5?.length > 0) {
+    CATEGORIES[4].title = field5;
+    CATEGORIES[4].query = query5;
+  }
+
+  const [customizing, setCustomizing] = useState(false);
+
+  const closeCustomize = () => {
+    setCustomizing(false);
+  };
+
   return (
     <div className="bg-black min-h-screen max-w-screen w-screen flex items-start justify-center p-4">
-      <div className="flex flex-col items-start justify-start gap-8 w-full md:w-[80%] p-6 md:p-10 md:pt-20">
-        {CATEGORIES.map((category) => (
-          <CategoryRow
-            key={category.title}
-            title={category.title}
-            query={category.query}
-          />
-        ))}
+      <div className="flex flex-col items-start justify-start gap-4 w-full md:w-[80%] p-6 md:p-10 md:pt-20">
+        <DiscoverCustomization />
+        <div className="flex flex-col items-start justify-start gap-8 w-full">
+          {CATEGORIES.map((category) => (
+            <CategoryRow
+              key={category.title}
+              title={category.title}
+              query={category.query}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/DiscoverCustomization.tsx
@@ -0,0 +1,232 @@
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
+import { Pencil } from "lucide-react";
+
+export const DiscoverCustomization = () => {
+  const router = useRouter();
+
+  // Saving and going to the Discover page
+  const [title1, setTitle1] = useState("");
+  const [query1, setQuery1] = useState("");
+  const [title2, setTitle2] = useState("");
+  const [query2, setQuery2] = useState("");
+  const [title3, setTitle3] = useState("");
+  const [query3, setQuery3] = useState("");
+  const [title4, setTitle4] = useState("");
+  const [query4, setQuery4] = useState("");
+  const [title5, setTitle5] = useState("");
+  const [query5, setQuery5] = useState("");
+
+  const saveAndDiscover = () => {
+    const params = new URLSearchParams();
+    if (title1) params.set("field1", title1);
+    if (query1) params.set("query1", query1);
+    if (title2) params.set("field2", title2);
+    if (query2) params.set("query2", query2);
+    if (title3) params.set("field3", title3);
+    if (query3) params.set("query3", query3);
+    if (title4) params.set("field4", title4);
+    if (query4) params.set("query4", query4);
+    if (title5) params.set("field5", title5);
+    if (query5) params.set("query5", query5);
+
+    router.push(`/discover?${params.toString()}`);
+  };
+
+  return (
+    <Drawer>
+      <DrawerTrigger className="text-white text-lg font-semibold duration-150 hover:text-white/80 flex gap-2 items-center justify-center cursor-pointer outline-none">
+        <Pencil color="currentColor" className="hidden md:inline" />
+        Customize
+      </DrawerTrigger>
+      <DrawerContent className="bg-black border-slate-600 text-white text-base! md:text-lg!">
+        <div className="mx-auto w-full md:max-w-1/2 px-6">
+          <DrawerHeader>
+            <DrawerTitle className=" md:text-2xl text-xl font-bold text-white">
+              Customize your discoveries!
+            </DrawerTitle>
+          </DrawerHeader>
+          <div className="flex flex-col gap-4 items-start justify-start w-full">
+            <div className="flex items-start justify-start gap-2">
+              <p className="md:text-lg text-base font-semibold">Title</p>
+              <p className="md:text-lg text-base font-normal text-slate-400">
+                What will be displayed on the Discover page as the title of the
+                section
+              </p>
+            </div>
+            <div className="flex items-start justify-start gap-2">
+              <p className="md:text-lg text-base font-semibold">Query</p>
+              <p className="md:text-lg text-base font-normal text-slate-400">
+                Describe what you'd like the section to contain, eg 'cozy fall
+                vibes' or 'best TikTok songs'
+              </p>
+            </div>
+          </div>
+          <Accordion type="single" collapsible className="gap-6 w-full mt-6">
+            <AccordionItem value="item-1" className="border-none">
+              <AccordionTrigger className="text-xl font-semibold cursor-pointer pt-0 pb-3">
+                Field 1
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="flex flex-col gap-2 items-start justify-start w-full md:w-1/2">
+                  <p className="md:text-lg text-base font-medium">Title 1</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="title-1"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={title1}
+                    onChange={(e) => setTitle1(e.target.value)}
+                  />
+                  <p className="md:text-lg text-base font-medium">Query 1</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="query-1"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={query1}
+                    onChange={(e) => setQuery1(e.target.value)}
+                  />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2" className="border-none">
+              <AccordionTrigger className="text-xl font-semibold cursor-pointer pt-0 pb-3">
+                Field 2
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="flex flex-col gap-2 items-start justify-start w-full md:w-1/2">
+                  <p className="md:text-lg text-base font-medium">Title 2</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="title-2"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={title2}
+                    onChange={(e) => setTitle2(e.target.value)}
+                  />
+                  <p className="md:text-lg text-base font-medium">Query 2</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="query-2"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={query2}
+                    onChange={(e) => setQuery2(e.target.value)}
+                  />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3" className="border-none">
+              <AccordionTrigger className="text-xl font-semibold cursor-pointer pt-0 pb-3">
+                Field 3
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="flex flex-col gap-2 items-start justify-start w-full md:w-1/2">
+                  <p className="md:text-lg text-base font-medium">Title 3</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="title-3"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={title3}
+                    onChange={(e) => setTitle3(e.target.value)}
+                  />
+                  <p className="md:text-lg text-base font-medium">Query 3</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="query-3"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={query3}
+                    onChange={(e) => setQuery3(e.target.value)}
+                  />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4" className="border-none">
+              <AccordionTrigger className="text-xl font-semibold cursor-pointer pt-0 pb-3">
+                Field 4
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="flex flex-col gap-2 items-start justify-start w-full md:w-1/2">
+                  <p className="md:text-lg text-base font-medium">Title 4</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="title-4"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={title4}
+                    onChange={(e) => setTitle4(e.target.value)}
+                  />
+                  <p className="md:text-lg text-base font-medium">Query 4</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="query-4"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={query4}
+                    onChange={(e) => setQuery4(e.target.value)}
+                  />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-5" className="border-none">
+              <AccordionTrigger className="text-xl font-semibold cursor-pointer pt-0 pb-3">
+                Field 5
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="flex flex-col gap-2 items-start justify-start w-full md:w-1/2">
+                  <p className="md:text-lg text-base font-medium">Title 5</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="title-5"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={title5}
+                    onChange={(e) => setTitle5(e.target.value)}
+                  />
+                  <p className="md:text-lg text-base font-medium">Query 5</p>
+                  <Input
+                    type="text"
+                    placeholder="Search for an artist"
+                    id="query-5"
+                    className="bg-black text-white text-lg! px-4 py-3 w-full"
+                    value={query5}
+                    onChange={(e) => setQuery5(e.target.value)}
+                  />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <DrawerFooter className="flex items-center justify-center gap-6 flex-row">
+            <DrawerClose className="cursor-pointer">Close</DrawerClose>
+            <button
+              className="cursor-pointer text-lg px-3 py-[4.3px] hover:bg-gray-300 transition bg-white rounded-md text-black"
+              onClick={() => saveAndDiscover()}
+            >
+              Save
+            </button>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
